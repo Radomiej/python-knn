0b062d617d53533e02989c1080e9d5cde0345cb7
Radomiej <radek1993-91@go2.pl>
2018-12-23 13:48:17 +0100

    refactoring(remove unused method)

--- Code/main.py
+++ Code/main.py
@@ -41,14 +41,4 @@ def main():
     knn_adapter.run()
 
 
-def is_number(n):
-    try:
-        float(n)  # Type-casting the string to `float`.
-        # If string is not a valid `float`,
-        # it'll raise `ValueError` exception
-    except ValueError:
-        return False
-    return True
-
-
 main()
