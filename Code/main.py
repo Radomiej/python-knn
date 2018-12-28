5fb3948c415ab5dfdcd8f1da03a8362737e5054f
Radomiej <radek1993-91@go2.pl>
2018-12-28 17:55:04 +0100

    fix console arguments

--- Code/main.py
+++ Code/main.py
@@ -15,16 +15,16 @@ command_setting = {
 def parse_arguments():
     # print('parse arguments')
     parser = argparse.ArgumentParser()
-    parser.add_argument("--k", type=int, default=5, help='Number of nearest neighbors. Default: 5')
-    parser.add_argument("--metric", type=str, default='euclid',
+    parser.add_argument("-k", type=int, default=5, help='Number of nearest neighbors. Default: 5')
+    parser.add_argument("-metric", type=str, default='euclid',
                         help='Metrics distance compute strategy: euclid(default) or manhattan')
-    parser.add_argument("--train", type=str, default='train',
+    parser.add_argument("-train", type=str, default='train',
                         help='Testing category: train(default), split, cross')
-    parser.add_argument("--percentage_split", type=float, default=0.25, help='Percentage split: (0,1) where 1 == 100%')
-    parser.add_argument("--cross_iteration", type=int, default=5,
+    parser.add_argument("-percentage_split", type=float, default=0.25, help='Percentage split: (0,1) where 1 == 100%')
+    parser.add_argument("-cross_iteration", type=int, default=5,
                         help='Cross iteration count also indicate how many part of set will be use like training and test data')
-    parser.add_argument("--decision_index", type=int, required=True, help='Index of column with decisions')
-    parser.add_argument("--file", type=str, required=True, help='File with data', )
+    parser.add_argument("-decision_index", type=int, default=-1, help='Index of column with decisions')
+    parser.add_argument("file", type=str, help='File with data', )
     args = parser.parse_args()
     command_setting['k'] = int(args.k)
     command_setting['metric'] = args.metric
